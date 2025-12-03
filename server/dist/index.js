e77c85918fa0170ae0739c658a641e3c27188afe
andrew-miroiu <andrew.miroiu@gmail.com>
2025-12-03 02:29:37 +0200

    Clean server deps + Node20 engine

--- server/dist/index.js
+++ server/dist/index.js
@@ -30,4 +30,5 @@ app.use('/comments', commentsRouter_1.default);
 app.use('/conversation', conversationRouter_1.default);
 app.use('/messages', messagesRouter_1.default);
 app.get('/', (_, res) => res.send(' Server is running'));
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
