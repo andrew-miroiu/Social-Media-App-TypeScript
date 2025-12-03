d7f6dc164a45afecbd6b1ae45e584c445049971a
andrew-miroiu <andrew.miroiu@gmail.com>
2025-12-03 02:03:29 +0200

    added port env

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,4 +30,6 @@ app.use('/messages', messagesRouter)
 
 app.get('/', (_, res) => res.send(' Server is running'))
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server running on http://localhost:5000'))
