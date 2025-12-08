ec19cb178e580b52077e9e8f1c6d1a78440fc990
andrew-miroiu <andrew.miroiu@gmail.com>
2025-12-08 18:42:03 +0200

    changed navbar icon

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg";
+import logo from "../assets/onlyfriends.svg";
 
 
 export default function Navbar({
